79c2d504e09b13f268a0583fee33acbd4da22a85
lexifuzzpup <lexifuzzpup@gmail.com>
2025-11-29 21:17:51 -0600

    improve color choices

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 function RGBtoHEX(r, g, b) {
-    return "&#" + r.toString(16).padStart(2, "0") + g.toString(16).padStart(2, "0") + b.toString(16).padStart(2, "0")
+    return "#" + r.toString(16).padStart(2, "0") + g.toString(16).padStart(2, "0") + b.toString(16).padStart(2, "0")
+}
+function HEXtoRGB(hex) {
+    const str = hex.replace("#", "");
+
+    const r = parseInt(str.slice(0, 2), 16);
+    const g = parseInt(str.slice(2, 4), 16);
+    const b = parseInt(str.slice(4, 6), 16);
+
+    return [ r, g, b ];
 }
 function HSVtoRGB(h, s, v) {
     var r, g, b, i, f, p, q, t;
@@ -25,12 +34,45 @@ function HSVtoRGB(h, s, v) {
         Math.round(b * 255)
     ];
 }
+function RGBtoHSV(r, g, b) {
+    if (arguments.length === 1) {
+        g = r.g, b = r.b, r = r.r;
+    }
+    var max = Math.max(r, g, b), min = Math.min(r, g, b),
+        d = max - min,
+        h,
+        s = (max === 0 ? 0 : d / max),
+        v = max / 255;
 
-function rainbowify(username) {
-    let outputName = ""
+    switch (max) {
+        case min: h = 0; break;
+        case r: h = (g - b) + d * (g < b ? 6: 0); h /= 6 * d; break;
+        case g: h = (b - r) + d * 2; h /= 6 * d; break;
+        case b: h = (r - g) + d * 4; h /= 6 * d; break;
+    }
+
+    return [ h, s, v ];
+}
+
+function rainbowify(username, from, to, linear) {
+    let outputName = document.createElement("span");
  
     for(let i = 0; i < username.length; i++) {
-        outputName += RGBtoHEX(...HSVtoRGB(i / username.length, 1, 1)) + username[i]
+        const t = (i / (username.length - 1));
+
+        const h = (to[0] - from[0]) * t + from[0];
+        const s = (to[1] - from[1]) * t + from[1];
+        const v = (to[2] - from[2]) * t + from[2];
+        const hex = linear ? RGBtoHEX(Math.floor(h), Math.floor(s), Math.floor(v)) : RGBtoHEX(...HSVtoRGB(h, s, v));
+
+        const section = document.createElement("span");
+        section.style.fontSize = "0px";
+        const char = document.createElement("span");
+        section.textContent = "&" + hex;
+        char.textContent = username[i];
+        char.style.color = hex;
+
+        outputName.append(section, char);
     }
     return outputName;
 }
@@ -38,7 +80,19 @@ function rainbowify(username) {
 const nameInput = document.querySelector("#name-input");
 const makeBtn = document.querySelector("#make");
 const nameOutput = document.querySelector("#output");
+const color1 = document.querySelector("#color1");
+const color2 = document.querySelector("#color2");
+const hueLinear = document.querySelector("#hue-linear");
 
 makeBtn.addEventListener("click", () => {
-    nameOutput.textContent = rainbowify(nameInput.value);
+    const linear = hueLinear.checked;
+    const color1rgb = HEXtoRGB(color1.value);
+    const color2rgb = HEXtoRGB(color2.value);
+
+    nameOutput.replaceChildren(rainbowify(
+        nameInput.value,
+        linear ? color1rgb : RGBtoHSV(...color1rgb),
+        linear ? color2rgb : RGBtoHSV(...color2rgb),
+        linear
+    ));
 })
